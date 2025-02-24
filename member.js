c8859c19f24e5dad0ccf2339baf380190a1035cf
ronpburton <71376360+ronpburton@users.noreply.github.com>
2025-02-24 08:24:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+  var member = new Member();
+  member.setName("skills");
+  member.setDescription("A member that is used to manage the skills of the bot.");
+  member.setIcon("https://i.imgure.com/4J0Y1qk.png");
